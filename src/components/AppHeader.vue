d6902914733c5d8903f1dbe995285ec7084133c4
William <william.9150@gmail.com>
2025-11-27 07:54:41 +0800

    fix: re-apply responsive hamburger menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,9 @@
           <img src="../assets/logo.jpg" alt="Palauan Tour" class="logo-img" />
         </router-link>
       </div>
-      <div class="nav-wrapper">
+
+      <!-- Desktop Menu -->
+      <div class="nav-wrapper desktop-menu">
         <el-menu mode="horizontal" :router="true" :default-active="$route.path" :ellipsis="false" class="nav-menu">
           <el-menu-item index="/">{{ $t('common.home') }}</el-menu-item>
           <el-sub-menu index="/about">
@@ -32,14 +34,41 @@
           </div>
         </el-menu>
       </div>
+
+      <!-- Mobile Menu Button -->
+      <div class="mobile-menu-btn" @click="drawerVisible = true">
+        <el-icon size="24"><Menu /></el-icon>
+      </div>
+
+      <!-- Mobile Drawer -->
+      <el-drawer v-model="drawerVisible" title="Menu" direction="rtl" size="80%">
+        <el-menu :router="true" :default-active="$route.path" class="mobile-nav-menu">
+          <el-menu-item index="/" @click="drawerVisible = false">{{ $t('common.home') }}</el-menu-item>
+          <el-sub-menu index="/about">
+            <template #title>{{ $t('common.about') }}</template>
+            <el-menu-item index="/about-us" @click="drawerVisible = false">{{ $t('common.about_us') }}</el-menu-item>
+            <el-menu-item index="/about-palau" @click="drawerVisible = false">{{ $t('common.about_palau') }}</el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="/tours" @click="drawerVisible = false">{{ $t('common.tours') }}</el-menu-item>
+          <el-menu-item index="/guide" @click="drawerVisible = false">{{ $t('common.guide') }}</el-menu-item>
+          <el-menu-item index="/booking" @click="drawerVisible = false">{{ $t('common.booking') }}</el-menu-item>
+        </el-menu>
+        <div class="mobile-lang-switch">
+          <el-button size="small" :type="locale === 'zh-TW' ? 'primary' : 'default'" @click="handleLangCommand('zh-TW')">繁體中文</el-button>
+          <el-button size="small" :type="locale === 'en-US' ? 'primary' : 'default'" @click="handleLangCommand('en-US')">English</el-button>
+        </div>
+      </el-drawer>
     </div>
   </el-header>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { ref } from 'vue'
+import { Menu } from '@element-plus/icons-vue'
 
 const { locale } = useI18n()
+const drawerVisible = ref(false)
 
 const handleLangCommand = (command: string) => {
   locale.value = command
@@ -86,5 +115,26 @@ const handleLangCommand = (command: string) => {
 .el-dropdown-link {
   display: flex;
   align-items: center;
+}
+
+/* Mobile Styles */
+.mobile-menu-btn {
+  display: none;
+  cursor: pointer;
+}
+.mobile-lang-switch {
+  margin-top: 20px;
+  display: flex;
+  gap: 10px;
+  justify-content: center;
+}
+
+@media (max-width: 768px) {
+  .desktop-menu {
+    display: none;
+  }
+  .mobile-menu-btn {
+    display: block;
+  }
 }
 </style>
